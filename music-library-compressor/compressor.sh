ec39aa47bc5657b41a9f11335c9804152d2d22f0
Rob Hitchens <rjhitchens3@gmail.com>
2025-03-21 22:49:46 -0400

    minor change to compressor.sh

--- music-library-compressor/compressor.sh
+++ music-library-compressor/compressor.sh
@@ -11,6 +11,46 @@ readonly supportedFormats=("flac" "wav")
 # FIXME: I don't think the below is used
 readonly supportedOutputFormats=("mp3")
 
+# PARAMS:
+# newDirectory ($1 string) - directory to write compressed file to.
+# originalFileName ($2 string) - the original name of the file.
+function compressFile {
+  # FIXME: need to review and fix
+  log debug "compressFile \$1: $1"
+  local newDirectory=$1
+  log debug "compressFile \$2: $2"
+  local originalFileName=$2
+  local filename=$(basename -- "$2")
+
+  log info "File: $filename"
+  local extension="${filename##*.}"
+  log info "File Extension: $extension"
+
+  if [[ $(echo "${supportedFormats[@]}" | grep -o "$extension" | wc -w) -eq 1]]; then
+    local targetFilePath="${newDirectory}/${filename%.*}.mp3"
+    if [[ -e $targetFilePath ]]; then
+      log info "Compressed file already exists ($targetFilePath) skipping."
+    else
+      log info "File: $filename matches supported formats. Compressing to target: $targetFilePath"
+
+      log trace "Executing: mkdir -p \"$newDirectory\""
+      mkdir -p "$newDirectory"
+
+      log trace "Executing: ffmpeg -i \"$i\" -ab 320k -map_metadata 0 -id3v2_version 3 \"$targetFilePath\""
+      yes | ffmpeg -i "$i" -ab 320k -map_metadata 0 -id3v2_version 3 "$targetFilePath"
+    fi
+  else
+    log warn "File: $filename does not match supported formats. copying"
+    targetFilePath="${newDirectory}/${filename}"
+    if [[ -e $targetFilePath ]]; then
+      log info "File already exists ($targetFilePath) skipping."
+    else
+      log info "Copying $originalFileName to $targetFilePath"
+      cp "$originalFileName" "$targetFilePath"
+    fi
+  fi
+}
+
 
 # Guard clause for inputs
 if [[ $# -ne 2 ]]; then
@@ -18,9 +58,9 @@ if [[ $# -ne 2 ]]; then
   exit 1
 fi
 
-log "info" "Provided directory $1"
-log "debug" "Listing subdirectories"
-log "trace" "changing to directory \"$1\""
+log info "Provided directory $1"
+log debug "Listing subdirectories"
+log trace "changing to directory \"$1\""
 cd "$1"
 
 readonly targetDirectory=$(echo "$2" | sed 's:/*$::')
@@ -39,7 +79,7 @@ for i in $files; do
   #      Then all I need to check for is whether or not the file type is compatible.
   #      Nevermind I still need to filter out directory strings.
   #      Also, need to filter out other miscellneous strings like values ending with $':'
-  log "info" "found /$i"
+  log info "found /$i"
 done
 readonly end=`date +%s`
-log "info" "Job took $(expr $end - $start) second(s) to run."
+log info "Job took $(expr $end - $start) second(s) to run."
