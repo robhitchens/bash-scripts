4f267fea71fb67977db5274fd333e9b4a5c07e52
Rob Hitchens <rjhitchens3@gmail.com>
2025-03-25 21:39:58 -0400

    Minor updated to logging to check for a global variable for setting logging level

--- utils/logger.sh
+++ utils/logger.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 readonly scriptName=$(basename "$0")
-
-# Note below use of `declare` is not portable
+# NOTE: below use of `declare` is not portable
 declare -A logLevels
 logLevels["INFO"]=5
 logLevels["WARN"]=4
@@ -10,8 +9,13 @@ logLevels["ERROR"]=3
 logLevels["DEBUG"]=2
 logLevels["TRACE"]=1
 
-# TODO want to pull logLevel from ENV default to INFO if not present
-loggingLevel="INFO"
+
+# FIXME should add guard to make sure logLevel is valid value.
+if [[ ! -z "$logLevel" ]]; then
+  loggingLevel=$(echo "$logLevel" | tr '[:lower:]' '[:upper:]')
+else
+  loggingLevel="INFO"
+fi
 
 # PARAMS: 
 # level ($1 string) - the level of the log message
