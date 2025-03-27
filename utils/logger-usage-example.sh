33a677f9d8654afc29714b2fe48070f72ee0f7aa
Rob Hitchens <rjhitchens3@gmail.com>
2025-03-26 20:17:29 -0400

    added logger usage example script

---
+++ utils/logger-usage-example.sh
@@ -0,0 +1,19 @@
+# NOTE: using alias within a bash script doesn't seem to work as I would think
+# alias log='bash ~/projects/bash-scripts/utils/logger.sh'
+
+function log { bash ~/projects/bash-scripts/utils/logger.sh "$@"; }
+
+log info "some info"
+log warn "some warning"
+log error "some error"
+log debug "some debug"
+log trace "some trace"
+
+# Set logLevel globally
+export logLevel=trace
+
+log info "some info again"
+log warn "some warning again"
+log error "some error again"
+log debug "some debug again"
+log trace "some trace again"
