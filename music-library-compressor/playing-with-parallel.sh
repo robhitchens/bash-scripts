44f07c0d0ef386847c6ce50e45b9f30b7093d37d
Rob Hitchens <rjhitchens3@gmail.com>
2025-04-11 14:16:26 -0400

    played around with parallel

---
+++ music-library-compressor/playing-with-parallel.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+function wait {
+  readonly seconds=5
+  readonly threadNumber="$1"
+  local i=0
+  while [[ "$i" -le seconds ]]; do
+    sleep 1s
+    echo "threadNumber $threadNumber sleeping $i..." | tee -a /tmp/parallel/processes
+    i=$((i + 1))
+  done
+}
+
+#wait "$1"
+export -f wait
+
+
+# <strike>using parallel like this doesn't seem to work, 
+# only the first iteration is executed and captured.</strike>
+# Okay so now it seems to work, but I don't know why
+parallel wait ::: {1..16}
+#echo "$parallelOutput"
