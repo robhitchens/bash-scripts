44ffd47d41fed888e22d949a281c19bd54a83f0e
Rob Hitchens <rjhitchens3@gmail.com>
2025-07-17 21:56:45 -0400

    minor change to discord updater

--- updaters/updateDiscord.sh
+++ updaters/updateDiscord.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mkdir -p /tmp/discord-update
-curl --output-dir "/tmp/discord-update"  -OL 'https://discord.com/api/download/stable?platform=linux&format=deb'
+curl --verbose --output-dir "/tmp/discord-update"  -OL 'https://discord.com/api/download/stable?platform=linux&format=deb'
 sudo dpkg -i /tmp/discord-update/stable 
