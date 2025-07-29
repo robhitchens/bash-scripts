52f9b5adfa7e75154c0ebaa3121938f8d9c1824d
robhitchens <>
2025-07-29 13:47:26 -0400

    playing around with the discogs api

--- cd-metadata-grabber/query-service.sh
+++ cd-metadata-grabber/query-service.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 # link to discogs api documentation <https://www.discogs.com/developers> 
 # example request
-echo $(curl -X 'GET' --location 'https://api.discogs.com/releases/249504') | jq '.'
+response=$(curl -i -X 'GET' --location 'https://api.discogs.com/releases/249504' --user-agent 'cd-mdg/0.1')
+lines=$(echo "$response" | wc -l)
+echo "$(echo "$response" | head -n$(($lines-1)))"
+echo "$response" | tail -n1 | jq '.'
