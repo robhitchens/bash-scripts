db3fc03291ff986e64cff4b81832d7d7f7a2fa8e
robhitchens <>
2025-07-29 13:49:27 -0400

    added usage of vendor media type to request

--- cd-metadata-grabber/query-service.sh
+++ cd-metadata-grabber/query-service.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # link to discogs api documentation <https://www.discogs.com/developers> 
 # example request
-response=$(curl -i -X 'GET' --location 'https://api.discogs.com/releases/249504' --user-agent 'cd-mdg/0.1')
+response=$(curl -i -H 'Accept: application/vnd.discogs.v2.discogs+json' -X 'GET' --location 'https://api.discogs.com/releases/249504' --user-agent 'cd-mdg/0.1')
 lines=$(echo "$response" | wc -l)
 echo "$(echo "$response" | head -n$(($lines-1)))"
 echo "$response" | tail -n1 | jq '.'
