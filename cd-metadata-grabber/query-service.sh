e825946d191149b5275f58532f9566aaad84644f
Rob Hitchens <rjhitchens3@gmail.com>
2025-09-03 19:41:23 -0400

    added request to get master album info from discogs to query-service script

--- cd-metadata-grabber/query-service.sh
+++ cd-metadata-grabber/query-service.sh
@@ -10,12 +10,12 @@ readonly barcode='0094636624822'
 readonly userAgent='cd-mdg/0.1'
 
 function fetchInitialData {
-  local barcode="$1"
+  local upc="$1"
   local response=$(curl -i \
     -H 'Accept: application/vnd.discogs.v2.discogs+json' \
     -H "$auth" \
     -X 'GET' \
-    --location "https://api.discogs.com/database/search?barcode=$barcode" \
+    --location "https://api.discogs.com/database/search?barcode=$upc" \
     --user-agent "$userAgent")
 
   local lines=$(echo "$response" | wc -l)
@@ -28,9 +28,23 @@ function fetchInitialData {
 function fetchTrackInfo {
   local url="$1"
 
-  # TODO curl master_url
+  local response=$(curl -i \
+    -H 'Accept: application/vnd.discogs.v2.discogs+json' \
+    -H "$auth" \
+    -X 'GET' \
+    --location "$url" \
+    --user-agent "$userAgent") 
+
+  local lines=$(echo "$response" | wc -l)
+  local responseHeaders=$(echo "$response" | head -n$(($lines-1)))
+  local json=$(echo "$response" | tail -n1 | \
+    jq '{id: .id, artists: .artists, title: .title, genres: .genres, styles: .styles, year: .year, tracklist: .tracklist}')
+
+  # TODO need to workout jq queries to reshape data in usable format for further processing  # TODO need to review format for tracklist metadata structure.
+  echo "$json"
   # TODO use jq to pull out artist info
   # TODO use jq to pull out label info
+  #      NOTE: it doesn't look like there's any label info on this request.
   # TODO use jq to pull out genre
   # TODO use jq to pull out album info
   # TODO use jq to pull track info and format into list of tracks
@@ -50,18 +64,19 @@ function fetchCoverImage {
     -H 'Accept: image/jpeg' \
     --location "$location" > "$absoluteFileOutput"
 
-  # TODO curl coverImage
-  # TODO write out coverImage file to location
 }
 
 readonly barcodeSearchResult=$(fetchInitialData "$barcode")
 # FIXME: kinda cheesing the logic a bit, searchresults will most likely be an array, and may contain more than one entry even for a single barcode. Should probably query some other data to fully match the correct result.
 #master_url=$(echo "$json" | jq '.results[0].master_url' | tr -d '"')
 readonly masterUrl=$(echo "$barcodeSearchResult" | jq '.results[0].master_url' | tr -d '"' )
+echo "master url: [$masterUrl]"
 #use '.results[0].cover_image' to get cover art as jpeg
-readonly coverImageUrl=$(echo "$barcodeSearchResult" | jq '.results[0].cover_image' | tr -d '"')
+#readonly coverImageUrl=$(echo "$barcodeSearchResult" | jq '.results[0].cover_image' | tr -d '"')
+#echo "cover image url: [$coverImageUrl]"
 
 readonly trackInfo=$(fetchTrackInfo "$masterUrl")
+echo "$trackInfo" 
 # TODO need to test this out.
 # fetchCoverImage "outputDir" "$coverImageUrl"
 
