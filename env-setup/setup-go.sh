18de3db32e641dfdefedf650bc58c4d80b4773af
Rob Hitchens <rjhitchens3@gmail.com>
2025-10-02 20:28:11 -0400

    updated go install script to include installation of gore (go repl)

--- env-setup/setup-go.sh
+++ env-setup/setup-go.sh
@@ -4,9 +4,12 @@
 # TODO should add variable for go version
 # TODO should add options to install go versions side by side
 rm -rf /usr/local/go && \
-tar -C /usr/local -xzvf go1.24.1.linux-amd64.tar.gz
+tar -C /usr/local -xzvf go1.24.3.linux-amd64.tar.gz
 # TODO should check to see if line already exists in bashrc
 cat "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
 # Install delve debugger
 go install github.com/go-delve/delve/cmd/dlv@latest
 cat "export PATH=$PATH:$HOME/go/bin" >> ~/.bashrc
+
+# Install gore repl
+go install github.com/x-motemen/gore/cmd/gore@latest
