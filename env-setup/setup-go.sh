015a3a1df94e362fcd96564c96225e2bded2dc81
Rob Hitchens <rjhitchens3@gmail.com>
2025-10-17 23:46:07 -0400

    made some tweaks to go install script, still needs some workshopping

--- env-setup/setup-go.sh
+++ env-setup/setup-go.sh
@@ -3,13 +3,17 @@
 # TODO should add steps to download and install go from source
 # TODO should add variable for go version
 # TODO should add options to install go versions side by side
-rm -rf /usr/local/go && \
-tar -C /usr/local -xzvf go1.24.3.linux-amd64.tar.gz
+# FIXME: forgot to curl command to download go binary.
+readonly goversion='go1.25.3.linux-amd64.tar.gz'
+cd ~/.local/share
+curl -L "https://go.dev/dl/${goversion}" -o "./${goversion}"
+rm -rf ~/.local/share/go && \
+tar -C ~/.local/share -xzvf "${goversion}"
 # TODO should check to see if line already exists in bashrc
-cat "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+cat "export PATH=\$PATH:$HOME/.local/share/go/bin" >> ~/.bashrc
 # Install delve debugger
 go install github.com/go-delve/delve/cmd/dlv@latest
-cat "export PATH=$PATH:$HOME/go/bin" >> ~/.bashrc
+cat "export PATH=\$PATH:$HOME/go/bin" >> ~/.bashrc
 
 # Install gore repl
 go install github.com/x-motemen/gore/cmd/gore@latest
