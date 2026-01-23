91add2da352c201eb43bdbd2e102437cff82d412
robhitchens <>
2026-01-23 08:45:54 -0500

    added actor-model sandbox to play around with actor model ideas

---
+++ actor-model/sandbox.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# TODO setup multiple concurrent processes that communicate via named pipes
+# TODO setup main loop process to step through functions, create multiple functions with support for a "step" argument and a message argument, messages should be written to queue files (read from beginning, write to end)
