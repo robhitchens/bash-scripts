20c963f4956dcdab922c53a4a6606ca0396dc87a
Rob Hitchens <rjhitchens3@gmail.com>
2026-04-07 21:30:16 -0400

    Started auto complete script for journal manager

---
+++ journal-manager/journmanager_auto_complete.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# TODO add install script that places in bash autocomplete directory
+
+function _journManager_auto_complete {
+	:
+}
+
+complete -F _journManager_auto_complete journal
