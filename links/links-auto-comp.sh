b6694a029066923c185289193dfc15b6db1d5ac1
robhitchens <>
2026-04-27 16:49:36 -0400

    started working on links auto complete script

--- links/links-auto-comp.sh
+++ links/links-auto-comp.sh
@@ -10,3 +10,16 @@ function getFriendlyNames {
 	# TODO if -f is not provided then default to $LINKSDOC var
 	# TODO exit if file is empty
 }
+
+function _links_auto {
+	local firstLevelOptions=('help' 'install' 'list' 'edit' '-h' '-w' '-c' '--help' '--file')
+	local word="${COMP_WORDS[COMP_CWORD]}"
+	# TODO figure out how I want autocomplete to work.
+	# --file can only be provided once
+	# argument after --file argument should be normal tab completion
+	# help, install, list, and edit can only be provided with the --file argument
+	# Only one of -w or -c can be provided
+	# Might trim down the different help options.
+}
+
+complete -F _links_auto links
