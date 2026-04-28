b676c2613eac298aa0a12c149a6ecde6c12e1502
Rob Hitchens <rjhitchens3@gmail.com>
2026-04-28 18:19:13 -0400

    Added comment to discord update script

--- updaters/updateDiscord.sh
+++ updaters/updateDiscord.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# TODO add options for deb package or tarball download and install
 mkdir -p /tmp/discord-update
 curl --verbose --output-dir "/tmp/discord-update"  -OL 'https://discord.com/api/download/stable?platform=linux&format=deb'
 sudo dpkg -i /tmp/discord-update/stable 
