60fe8aec10dffb7c396628ba05e09a8e227d5984
robhitchens <>
2026-04-28 14:50:10 -0400

    Added script for git commit message template

---
+++ snippets/git-commit-template.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# TODO add basic string templating
+# TODO add install script
+# Alternatively could work on a simple vim script function to poop out template
+echo ":changeNumber:: :title:
+
+Why:
+
+What:
+"
