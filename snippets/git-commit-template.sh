0ba502c3d9784873dfabaf38d098b06a9499b66b
robhitchens <>
2026-06-09 09:50:28 -0400

    Added more to git-commit-template.
    Logic for help doc and install partially broken

--- snippets/git-commit-template.sh
+++ snippets/git-commit-template.sh
@@ -3,25 +3,71 @@
 # TODO add install script
 # Alternatively could work on a simple vim script function to poop out template
 
-declare -g changeNumber="" title="" why="" what=()
+declare -g changeNumber="" title="" why="" what=() echoHelp=false install=false
+
+function fullDoc {
+	cat <<EOF
+Usage:
+  git-commit-template [OPTS]
+  gct [OPTS]
+
+Synopsis:
+  git-commit-template poops out a formatted template to stdout
+
+Description:
+
+Options:
+  -h|--help                             Prints help doc to stdout
+  -i|--install                          Installs script under /usr/local/bin
+  -c|--changeNumber
+  -t|--title
+  -W|--why
+  -w|--what
+EOF
+}
 
 function parseArgs {
 	# TODO should probably add some guards against using mixed flags or providing the same flag twice.
+	if (($# == 0)); then
+		echoHelp=true
+		return 0
+	fi
+
 	for ((i = 0; i < $#; i++)); do
 		if [[ "${!i}" =~ ^--(.+)$ ]]; then
 			local arg="${BASH_REMATCH[1]}"
 			case "$arg" in
+			help)
+				echoHelp=true
+				return 0
+				;;
+			install)
+				install=true
+				return 0
+				;;
 			changeNumber)
 				((i++))
-				changeNumber="${!i}"
+				if [[ -z "$changeNumber" ]]; then
+					changeNumber="${!i}"
+				else
+					echo "changeNumber already set" >&2
+				fi
 				;;
 			title)
 				((i++))
-				title="${!i}"
+				if [[ -z "$title" ]]; then
+					title="${!i}"
+				else
+					echo "title already set" >&2
+				fi
 				;;
 			why)
 				((i++))
-				why="${!i}"
+				if [[ -z "$why" ]]; then
+					why="${!i}"
+				else
+					echo "why already set" >&2
+				fi
 				;;
 			what)
 				((i++))
@@ -35,17 +81,37 @@ function parseArgs {
 		if [[ "${!i}" =~ ^-(.+)$ ]]; then
 			local arg="${BASH_REMATCH[1]}"
 			case "$arg" in
+			h)
+				echoHelp=true
+				return 0
+				;;
+			i)
+				install=true
+				return 0
+				;;
 			c)
 				((i++))
-				changeNumber="${!i}"
+				if [[ -z "$changeNumber" ]]; then
+					changeNumber="${!i}"
+				else
+					echo "changeNumber already set" >&2
+				fi
 				;;
 			t)
 				((i++))
-				title="${!i}"
+				if [[ -z "$title" ]]; then
+					title="${!i}"
+				else
+					echo "title already set" >&2
+				fi
 				;;
 			W)
 				((i++))
-				why="${!i}"
+				if [[ -z "$why" ]]; then
+					why="${!i}"
+				else
+					echo "why already set" >&2
+				fi
 				;;
 			w)
 				((i++))
@@ -62,8 +128,33 @@ function formatWhats {
 	done
 }
 
+function installScript {
+	local symlink='/usr/local/bin/git-commit-template'
+	local symlink_short='/usr/local/bin/gct'
+	# TODO should probably prompt user before nuking existing symlink file.
+	if [[ -f $symlink || -f $symlink_short ]]; then
+		echo "Removing existing links: $symlink, $symlink_short" >&2
+		rm -f $symlink $symlink_short
+	else
+		local scriptLocation=$(find . -type f -iname 'git-commit-template.sh' | xargs realpath --relative-to=/ | sed -E 's/(.*)/\/\1/')
+		# TODO should error out if script can't be found.
+		echo "Adding symlink: $symlink"
+		ln -s $scriptLocation $symlink
+		echo "Adding symlink: $symlink_short"
+		ln -s $scriptLocation $symlink_short
+	fi
+}
+
 function main {
 	parseArgs "$@"
+	if [[ "$echoHelp" == "true" ]]; then
+		fullDoc
+		return 0
+	fi
+	if [[ "$installScript" == "true" ]]; then
+		install
+		return 0
+	fi
 	local template=":changeNumber:: :title:
 
 Why:
@@ -80,6 +171,7 @@ What:
 	if [[ -n "$title" ]]; then
 		template="${template/:title:/$title}"
 	fi
+	# TODO add length validation to title
 	if [[ -n "$why" ]]; then
 		template="${template/:why:/$why}"
 	fi
